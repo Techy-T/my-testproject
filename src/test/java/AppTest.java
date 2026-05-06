31b23c5ec314885c8b6d982901e9d34354ce7614
TU <umt.techy@gmail.com>
2026-05-06 17:00:10 -0400

    update code on java test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,16 +1,15 @@
 import com.example.App;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class AppTest {
+class AppTest {
 
     @Test
-    public void testApp() {
+    void testApp() {
         App myApp = new App();
 
         String result = myApp.getStatus();
 
         assertEquals("OK", result);
     }
-
 }
